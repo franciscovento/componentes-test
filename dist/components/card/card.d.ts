c2b619e28e506a326eb7c9c90d010b4c236518ae
EGO 85# <francisco.vento@egodesign.io>
2024-11-08 15:57:45 -0300

    feat: add dist to repo

---
+++ dist/components/card/card.d.ts
@@ -0,0 +1,6 @@
+type CardProps = {
+    title: string;
+    description: string;
+};
+export declare const Card: ({ title, description }: CardProps) => import("react/jsx-runtime").JSX.Element;
+export {};
