cdb092987e9d0787d029c85a42149859a2cbd5cf
EGO 85# <francisco.vento@egodesign.io>
2024-11-28 13:10:06 -0300

    fix: app provider with optional

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { App as AntdApp, AppProps } from 'antd';
 interface Props {
   children: React.ReactNode;
-  appProps: AppProps
+  appProps?: AppProps
 }
 export const App:FC<Props> = ({children, appProps}) => {
   return <AntdApp {...appProps}  >{children}</AntdApp>
